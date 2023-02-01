43c0a40ccab4d2981c7bc5da90bc83569300bd39
FelipeRodriguees <contactfeliper@gmail.com>
2023-01-31 22:46:01 -0300

    Project started.

---
+++ src/main/java/com/example/reportswiththymeleaf/ReportsWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportswiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsWithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reportswiththymeleaf/ReportsWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportswiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
